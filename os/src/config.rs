0926590c127528649342717406516fa175add6dc
benzil <huangzhaoxiang@mail.nwpu.edu.cn>
2022-08-09 17:42:14 +0800

    Adjust `KERNEL_HEAP_SIZE` & `MEMORY_END`.

--- os/src/config.rs
+++ os/src/config.rs
@@ -7,21 +7,19 @@ pub const USER_STACK_SIZE: usize = PAGE_SIZE * 40;
 pub const USER_HEAP_SIZE: usize = PAGE_SIZE * 20;
 
 pub const KERNEL_STACK_SIZE: usize = PAGE_SIZE * 2;
-#[cfg(not(feature = "board_fu740"))]
-pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x240;
-#[cfg(feature = "board_fu740")]
+#[cfg(feature = "board_k210")]
+pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x200;
+#[cfg(not(feature = "board_k210"))]
 pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x2000;
 pub const MMAP_BASE: usize = 0x6000_0000;
 pub const MMAP_END: usize = 0x8000_0000;
 
 // manually make usable memory space equal
 pub const MEMORY_START: usize = 0x8000_0000;
-#[cfg(all(not(feature = "board_k210"), not(feature = "board_fu740")))]
-pub const MEMORY_END: usize = 0x809e_0000;
 #[cfg(feature = "board_k210")]
 pub const MEMORY_END: usize = 0x8080_0000;
-#[cfg(feature = "board_fu740")]
-pub const MEMORY_END: usize = 0x9000_0000;
+#[cfg(not(feature = "board_k210"))]
+pub const MEMORY_END: usize = 0x8800_0000;
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 0xc;
 
