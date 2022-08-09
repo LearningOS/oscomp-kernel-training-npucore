977c3fe058e2d2ced63a8fabfdb1d992ca83cc64
benzil <huangzhaoxiang@mail.nwpu.edu.cn>
2022-08-10 01:38:02 +0800

    Adjust `CLOCK_FREQ` to 1MHz according to the manual & keep consistent with other teams.

--- os/src/boards/fu740.rs
+++ os/src/boards/fu740.rs
@@ -1,6 +1,11 @@
 use crate::config::DISK_IMAGE_BASE;
 
-pub const CLOCK_FREQ: usize = 1_200_000;
+/// should be 1MHz.
+/// # Reference
+/// https://www.starfivetech.com/uploads/fu740-c000-manual-v1p2.pdf, page 76. \
+/// "The CPU real time clock (rtcclk) runs at **1 MHz** and is driven from input pin RTCCLKIN. This
+/// should be connected to an external oscillator."
+pub const CLOCK_FREQ: usize = 1_000_000;
 
 pub const MMIO: &[(usize, usize)] = &[
     (DISK_IMAGE_BASE, 0x800_0000), // disk image
