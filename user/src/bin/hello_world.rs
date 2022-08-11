e84e1d8cd9bb93c6a4908f87c5f84b86a3a5a98b
gsd <913887524@qq.com>
2022-08-11 18:46:10 +0800

    PPT feature: PS1 enviroment and hello world

--- user/src/bin/hello_world.rs
+++ user/src/bin/hello_world.rs
@@ -6,6 +6,8 @@ extern crate user_lib;
 
 #[no_mangle]
 pub fn main() -> i32 {
-    println!("Hello world from user mode program!");
+    println!("\x1b[1m\x1b[93mHello! It's NPUCore\x1b[0m");
+    println!("\x1b[1m\x1b[34mSchool\x1b[0m: Northwestern Polytechnical University");
+    println!("\x1b[1m\x1b[34mDeveloper\x1b[0m: Zhaoxiang Huang, Yuxuan Lin, Sundi Guan");
     0
 }
